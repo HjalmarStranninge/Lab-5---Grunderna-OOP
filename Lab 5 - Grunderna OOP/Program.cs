e8608a81fa569c7a723280fb8877da4bfbdbd196
Hjalmar Stranninge <Hjalmar.stranninge@outlook.com>
2023-09-26 11:25:12 +0200

    Bytte ut cirkel mot sfar

--- Lab 5 - Grunderna OOP/Program.cs
+++ Lab 5 - Grunderna OOP/Program.cs
@@ -1,30 +1,37 @@
 ﻿namespace Lab_5___Grunderna_OOP
 {
-    class Circle
+    class Sphere
     {
         float _pi = 3.141f;
         int _Radius;
 
-        public Circle(int radius)
+        public Sphere(int radius)
         {
             _Radius = radius;
         }
 
         public void getArea()
         {
-            float area = _Radius * _Radius * _pi;
-            Console.WriteLine($"Cirkelns area ar {area}!");
+            float area = 4 * _pi * (_Radius*_Radius);
+            Console.WriteLine($"Sfarens area ar {area} kvadratcentimeter!");
+        }
+        public void getVolume()
+        {
+            float volume = (4/3)*_pi * (_Radius * _Radius *_Radius);
+            Console.WriteLine($"Sfarens volym ar {volume} kubikcentimeter!");
         }
     }
     internal class Program
     {
         static void Main(string[] args)
         {
-            Circle firstCircle = new Circle(5);
-            firstCircle.getArea();
+            Sphere firstSphere = new Sphere(5);
+            firstSphere.getArea();
+            firstSphere.getVolume();
 
-            Circle secondCircle = new Circle(6);
-            secondCircle.getArea();
+            Sphere secondSphere = new Sphere(6);
+            secondSphere.getArea();
+            secondSphere.getVolume();
         }
     }
 }
